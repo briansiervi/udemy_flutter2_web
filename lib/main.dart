7162c93a99d7904a4ad845b4c967b71867ee7efb
Brian <brianmazini@gmail.com>
2021-10-29 16:26:57 -0300

    habilitando o device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:device_preview/plugins.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_aula/diferentes_tamanhos.dart';
 import 'package:flutter_web_aula/loja_virtual.dart';
@@ -10,17 +12,27 @@ import 'package:flutter_web_aula/tamanho_proporcional.dart';
 import 'package:flutter_web_aula/tamanho_textos.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Flutter Web",
-    debugShowCheckedModeBanner: false,
-    //home: ResponsividadeMediaQuery(),
-    //home: ResponsividadeRowCol(),
-    //home: ResponsividadeWrap(),
-    //home: Orientacao(),
-    //home: RegrasLayout(),
-    //home: LojaVirtual(),
-    //home: TamanhoTextos(),
-    //home: TamanhoProporcional(),
-    home: DiferentesTamanhos(),
-  ));
+  runApp(DevicePreview(
+      enabled: true,
+      plugins: const [
+        ScreenshotPlugin(),
+        FileExplorerPlugin(),
+        SharedPreferencesExplorerPlugin(),
+      ],
+      builder: (context) {
+        return MaterialApp(
+          title: "Flutter Web",
+          debugShowCheckedModeBanner: false,
+          useInheritedMediaQuery: true,
+          //home: ResponsividadeMediaQuery(),
+          //home: ResponsividadeRowCol(),
+          //home: ResponsividadeWrap(),
+          //home: Orientacao(),
+          //home: RegrasLayout(),
+          home: LojaVirtual(),
+          //home: TamanhoTextos(),
+          //home: TamanhoProporcional(),
+          //home: DiferentesTamanhos(),
+        );
+      }));
 }
